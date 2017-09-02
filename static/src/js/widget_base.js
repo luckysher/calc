a6887a22c292b54415cb78df95a7260d9ee83d2c
luckysher <luckyshera95@gmail.com>
2017-09-02 17:27:46 +0530

    added base  wigets for class

--- static/src/js/widget_base.js
+++ static/src/js/widget_base.js
@@ -3,11 +3,10 @@ function openerp_calc_basewidget(instance, module){ //module is instance.calc
 
     module.CalcBaseWidget = instance.web.Widget.extend({
         init:function(parent,options){
-            console.log("CALC BASE(INIT) >>---->   ......''''.... ");
             this._super(parent);
             options = options || {};
             this.calc = options.calc || (parent ? parent.calc : undefined);
             this.calc_widget = options.calc_widget || (parent ? parent.calc_widget : undefined);
-        },
+        }
     });
 }
