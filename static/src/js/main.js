0ca9cb4feba2204e4c6d7c58b44c7b2f30b11317
luckysher <luckyshera95@gmail.com>
2017-11-17 12:52:04 +0530

    updated main js file

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -2,23 +2,9 @@
 openerp.calc = function(instance) {
 
     instance.calc = {};
-
     var module = instance.calc;
-
-    //openerp_calc_db(instance,module);         // import db.js
-
-    //openerp_calc_models(instance,module);     // import calc_models.js
-
-    openerp_calc_basewidget(instance,module); // import calc_basewidget.js
-
-    //openerp_calc_keyboard(instance,module);   // import  calc_keyboard_widget.js
-
-    //openerp_calc_screens(instance,module);    // import calc_screens.js
-
-    //openerp_calc_devices(instance,module);    // import calc_devices.js
-
-    openerp_calc_widgets(instance,module);    // import calc_widgets.js
-
+    openerp_calc_basewidget(instance,module);
+    openerp_calc_widgets(instance,module);
     instance.web.client_actions.add('calc.ui', 'instance.calc.CalcWidget');
 };
 
