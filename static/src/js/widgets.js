362e6f23250f8e0799e9793c0966f6a6bf31b422
luckysher <luckyshera95@gmail.com>
2017-12-11 16:15:28 +0530

    added var for solving equations

--- static/src/js/widgets.js
+++ static/src/js/widgets.js
@@ -1,15 +1,19 @@
 function openerp_calc_widgets(instance, module){
     var QWeb = instance.web.qweb;
 	var _t = instance.web._t;
+	var num1 = null;
+    var num2 = null;
+    var op_plus = false;
+    var op_min = false;
+    var op_ml = false;
+    var op_div = false;
 
     module.CalcWidget = module.CalcBaseWidget.extend({
         template: 'NumpadWidget',
         init: function(parent, options) {
             this._super(parent, options);
             this.calc_widget = this; //So that calc_widget's childs have child_widget set automatically
-            this.show_chars = function (){
 
-            };
             this.getTextboxText = function (){
                 return this.$el.find('.input-box').val();
             };
@@ -28,13 +32,19 @@ function openerp_calc_widgets(instance, module){
             };
             this.solveEquation = function(equation){
                 var result;
-                
+                var oprts = ['/', '*', '+', '-'];
+                //$.each(oprts, function(){
+
+                //});
+                console.log("Solving equation..." + equation);
                 return result;
             };
         },
 
         start: function() {
             var self = this;
+
+
             $('.numpad button').bind('click', function(){
                 var val = $(this).text();
                 if (this.id == "back"){
@@ -47,15 +57,17 @@ function openerp_calc_widgets(instance, module){
                 }
                 if (this.id == "eq"){
                     var equation = self.getTextboxText();
-                    if (equation.indexOf(""))
-                    self.setTextboxText("");
+                    //if (equation.indexOf(""))
+                    console.log("var op_min = " + op_min);
+                    self.solveEquation("a+b");
+                    //self.setTextboxText("");
                     return;
                 }
-
+              
                  val = self.getTextboxText() + val;
                  console.log("Text is: " + val);
-                 self.setTextboxText(val);
 
+                 self.setTextboxText(val);
             });
            }
         });
